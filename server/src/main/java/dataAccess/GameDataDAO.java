02f2dcd93ac4ef72ca3dc1a743762f83d91d946f
Darxxn <stonelai11@gmail.com>
2024-03-07 16:36:27 -0700

    Added (GameDataDAO) throw statements

--- server/src/main/java/dataAccess/GameDataDAO.java
+++ server/src/main/java/dataAccess/GameDataDAO.java
@@ -2,11 +2,9 @@ package dataAccess;
 
 import model.GameData;
 
-import java.util.ArrayList;
-
 public interface GameDataDAO {
-    void createGame(GameData game);
-    GameData readGame(Integer gameID);
-    void deleteGame(Integer gameID);
-    void deleteAllGames();
+    void createGame(GameData game) throws DataAccessException;
+    GameData readGame(Integer gameID) throws DataAccessException;
+    void deleteGame(Integer gameID) throws DataAccessException;
+    void deleteAllGames() throws DataAccessException;
 }
