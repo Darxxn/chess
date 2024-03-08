e096ed51bbee2e34d4c75ea30df87943b4eb12b9
Darxxn <stonelai11@gmail.com>
2024-03-07 18:33:14 -0700

    Delete (GameDataDAO) whiteSpace

--- server/src/main/java/dataAccess/GameDataDAO.java
+++ server/src/main/java/dataAccess/GameDataDAO.java
@@ -3,7 +3,6 @@ package dataAccess;
 import model.GameData;
 import java.util.ArrayList;
 
-
 public interface GameDataDAO {
     void createGame(GameData game) throws DataAccessException;
     GameData readGame(Integer gameID) throws DataAccessException;
