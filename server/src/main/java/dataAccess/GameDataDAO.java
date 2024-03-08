6b0f51264c492aa5a4fef5ceb2de21ce4a9483a1
Darxxn <stonelai11@gmail.com>
2024-03-07 17:47:43 -0700

    Added (GameDataDAO) ArrayList to readAllGames

--- server/src/main/java/dataAccess/GameDataDAO.java
+++ server/src/main/java/dataAccess/GameDataDAO.java
@@ -1,10 +1,13 @@
 package dataAccess;
 
 import model.GameData;
+import java.util.ArrayList;
+
 
 public interface GameDataDAO {
     void createGame(GameData game) throws DataAccessException;
     GameData readGame(Integer gameID) throws DataAccessException;
     void deleteGame(Integer gameID) throws DataAccessException;
     void deleteAllGames() throws DataAccessException;
+    ArrayList<GameData> readAllGames() throws DataAccessException;
 }
