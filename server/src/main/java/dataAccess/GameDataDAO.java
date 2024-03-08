14a910b38995156c6d53512478dd7a6a88b30fc6
Darxxn <stonelai11@gmail.com>
2024-03-07 18:31:00 -0700

    Rename (GameDataDAO) readAllGames and deleteAllGames

--- server/src/main/java/dataAccess/GameDataDAO.java
+++ server/src/main/java/dataAccess/GameDataDAO.java
@@ -8,6 +8,6 @@ public interface GameDataDAO {
     void createGame(GameData game) throws DataAccessException;
     GameData readGame(Integer gameID) throws DataAccessException;
     void deleteGame(Integer gameID) throws DataAccessException;
-    void deleteAllGames() throws DataAccessException;
-    ArrayList<GameData> readAllGames() throws DataAccessException;
+    void deleteAllGameData() throws DataAccessException;
+    ArrayList<GameData> allGames() throws DataAccessException;
 }
