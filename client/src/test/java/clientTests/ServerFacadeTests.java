7ca0714eed583be01eb3bdf1b5a39ef5a3a60b83
Darxxn <stonelai11@gmail.com>
2024-03-25 19:46:00 -0600

    Finished (testCases) writing testCases

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,10 +1,12 @@
-package java.clientTests;
+package clientTests;
 
 import dataAccess.DataAccessException;
 import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 import ui.*;
+
+import javax.xml.crypto.Data;
 
 
 public class ServerFacadeTests {
@@ -14,10 +16,10 @@ public class ServerFacadeTests {
 
     @BeforeAll
     public static void init() {
-        var port = 1234;
+        var port = 8080;
         server = new Server();
         server.run(port);
-        serverFacade = new ServerFacade("http://localhost:1234");
+        serverFacade = new ServerFacade("http://localhost:8080");
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -32,14 +34,8 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
-    }
-
-    @Test
-    public void positiveRegister() {
+    public void positiveRegisterUser() {
         String username = "newUser";
         String password = "newPass123";
         String email = "newEmail@example.com";
@@ -51,6 +47,215 @@ public class ServerFacadeTests {
         } catch (DataAccessException ex) {
             Assertions.fail("Registration failed unexpectedly: " + ex.getMessage());
         }
-        Assertions.assertNotNull(authResult, "AuthInfo should not be null after successful registration.");
+        Assertions.assertNotNull(authResult, "AuthData should not be null after successful registration.");
+    }
+
+    @Test
+    public void negativeRegisterUser() {
+        String username = null;
+        String password = "newPass123";
+        String email = "newEmail@example.com";
+
+        try {
+            serverFacade.registerUser(username, password, email);
+            Assertions.fail("Expected exception");
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeRegisteringExistingUser() {
+        String username = "existingUser";
+        String password = "eUser";
+        String email = "eUser@example.com";
+
+        try {
+            serverFacade.registerUser(username, password, email);
+            serverFacade.registerUser(username, password, email);
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void positiveLoginUser() {
+        String username = "newUser";
+        String password = "newPass123";
+        String email = "newEmail@example.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            serverFacade.logout(authData.authToken());
+
+            AuthData test = serverFacade.login(username, password);
+
+            Assertions.assertNotNull(test);
+
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeLoginUser() {
+        String username = "newUser";
+        String password = "newPass123";
+        String email = "newEmail@example.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            serverFacade.logout(authData.authToken());
+
+            serverFacade.login(username, null);
+
+            Assertions.fail("Cannot login user");
+
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void positiveLogoutUser() {
+        String username = "newUser";
+        String password = "newPass123";
+        String email = "newEmail@example.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            serverFacade.logout(authData.authToken());
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeLogoutUser() {
+        String username = "newUser";
+        String password = "newPass123";
+        String email = "newEmail@example.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            serverFacade.logout(authData.authToken());
+            serverFacade.logout(authData.authToken());
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void positiveListGames() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame(authData.authToken(), "testGame1");
+            serverFacade.createGame(authData.authToken(), "testGame2");
+            serverFacade.createGame(authData.authToken(), "testGame3");
+            var games = serverFacade.listGames(authData.authToken());
+            Assertions.assertNotNull(games);
+        } catch (DataAccessException e) {
+            Assertions.fail("Error" + e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeListGames() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame(authData.authToken(), "testGame1");
+            serverFacade.createGame(authData.authToken(), "testGame2");
+            serverFacade.createGame(authData.authToken(), "testGame3");
+            serverFacade.listGames("wrongToken");
+            Assertions.fail("Expected exception");
+        }
+        catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void positiveCreateGame() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame(authData.authToken(), "testGame1");
+            var games = serverFacade.listGames(authData.authToken());
+            Assertions.assertNotNull(games);
+        } catch (DataAccessException e) {
+            Assertions.fail("Error\n" + e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeCreateGame() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame("Wrong Token", "testGame1");
+            Assertions.fail("Expected exception");
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
+    }
+
+    @Test
+    public void positiveJoinGame() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame(authData.authToken(), "testGame1");
+            var games = serverFacade.listGames(authData.authToken());
+            Assertions.assertNotNull(games);
+            var result = serverFacade.joinGame(authData.authToken(), games.games().get(0).gameID(), "white");
+            Assertions.assertNotNull(result);
+        } catch (DataAccessException e) {
+            Assertions.fail("Error\n" + e.getMessage());
+        }
+    }
+
+    @Test
+    public void negativeJoinGame() {
+        String username = "testUser";
+        String password = "testPassword";
+        String email = "test@test.com";
+
+
+        try {
+            AuthData authData = serverFacade.registerUser(username, password, email);
+            Assertions.assertNotNull(authData);
+            serverFacade.createGame(authData.authToken(), "testGame1");
+            var games = serverFacade.listGames(authData.authToken());
+            Assertions.assertNotNull(games);
+            serverFacade.joinGame(authData.authToken(), -1, "white");
+            Assertions.fail("Expected exception");
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error\n", e.getMessage());
+        }
     }
 }
