a37b68b90063e4d02be95aeb8992fb9cb1a031c1
Darxxn <stonelai11@gmail.com>
2024-04-09 13:09:09 -0600

    Added (Phase 6) WSFacade

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -1,6 +1,11 @@
 package ui;
 
+import exception.ResponseException;
+
 import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
     private Session session;
@@ -19,6 +24,26 @@ public class WebSocketFacade extends Endpoint {
     public void onError(){
     }
 
+    public WebSocketFacade(String url, MakeBoard game) throws ResponseException {
+        try {
+            this.game = game;
+            url = url.replace("http://", "ws://") + "/connect";
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, new URI(url));
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    returnedMessage(message);
+                }
+            });
+        }
+        catch (DeploymentException | IOException | URISyntaxException exception) {
+            throw new ResponseException(500, "Failed to connect to the server");
+        }
+    }
 
+    public void returnedMessage(String message) {
+
+    }
 
 }
