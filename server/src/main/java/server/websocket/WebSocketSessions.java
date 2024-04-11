ac33aea4a670ea59786a1384868b429c4ec8cfc5
Darxxn <stonelai11@gmail.com>
2024-04-10 23:36:52 -0600

    Added WebSocketSessions for SQL

---
+++ server/src/main/java/server/websocket/WebSocketSessions.java
@@ -0,0 +1,39 @@
+package server.websocket;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.eclipse.jetty.websocket.api.Session;
+
+public class WebSocketSessions {
+
+    private Map<Integer, Map<String, Session>> sessionMap;
+
+    public WebSocketSessions() {
+        sessionMap = new HashMap<>();
+    }
+
+    public void addSessionToGame(Integer gameID, String authToken, Session session) {
+        if (sessionMap.containsKey(gameID)) {
+            sessionMap.get(gameID).put(authToken, session);
+        } else {
+            sessionMap.put(gameID, new HashMap<>(Map.of(authToken, session)));
+        }
+    }
+
+    public void removeSessionFromGame(Integer gameID, String authToken, Session session) {
+        if (sessionMap.containsKey(gameID)) {
+            sessionMap.get(gameID).remove(authToken);
+        }
+    }
+
+    public void removeSession(Session session) {
+        for (Map<String, Session> map : sessionMap.values()) {
+            map.values().remove(session);
+        }
+    }
+
+    public Map<String, Session> getSessionsForGame(Integer gameID) {
+        return sessionMap.get(gameID);
+    }
+
+}
